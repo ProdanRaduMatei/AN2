32d4b354acbf6f1f6adbbc45398289ff35257d43
ProdanRaduMatei <mateiprodan1@gmail.com>
2023-12-15 09:13:42 +0200

    15.12.2023

--- Sem 3/Arhitectura Sistemelor de Calcul/asm_tools/npp/Lab5EmaA13.asm
+++ Sem 3/Arhitectura Sistemelor de Calcul/asm_tools/npp/Lab5EmaA13.asm
@@ -11,37 +11,33 @@ import exit msvcrt.dll    ; exit is a function that ends the calling process. It
 ; our data is declared here (the variables needed by our program)
 segment data use32 class=data
     ; ...
-    ;A string of bytes A is given. Construct string B containing only odd values from string A.
-    ;If A = 17, 4, 2, -2, -1 => B = 17, -1
+    ; A string of bytes A is given. Construct string B containing only the values divisible with 6 from string A.
+    ;If A = 12, 13, 14, 18 => B = 12, 18
 
-    A db 17, 4, 2, -2, -1
+    A db 12, 13, 14, 18
     ls equ $ - A
-   
     B resb ls
-       
 
 ; our code starts here
 segment code use32 class=code
     start:
-    mov esi, 0
-    mov edi, 0
-    mov ecx, ls
-    mov bl, 2 
         ; ...
+        mov esi, 0
+        mov edi, 0
+        mov ecx, ls
+        mov bl, 6
         repeta:
-            movsx ax, byte [A + esi] ; ax = A[esi]
-            idiv bl ; al = ax / bl ; ah = ax % bl
-            cmp ah, 1 ; ah == 1
-            je odd ; if ah == 1 jump to odd
-            jmp end
-        odd:
-            mov al, [A+esi]
-            mov byte [B + edi], al ; B[edi] = al
-            inc edi ; edi++
-        end:
-         inc esi ; esi++
-         loop repeta
-        ; ...
+            movsx ax, byte [A + esi]; ax = A[esi]
+            idiv bl; ax = A[esi] / bl; ah = A[esi] % bl
+            cmp ah, 0; ah== 0?
+            je divizibil; yes -> jump to divizibil
+            jmp final
+        divizibil: 
+            mov [B + edi], byte [A + esi]; B[edi] = A[esi]
+            inc edi; edi++
+        final:
+            inc esi; esi++
+            loop repeta
         ; exit(0)
         push    dword 0      ; push the parameter for exit onto the stack
         call    [exit]       ; call exit to terminate the program
--- Sem 3/Arhitectura Sistemelor de Calcul/asm_tools/npp/Lab5EmaB3.asm
+++ Sem 3/Arhitectura Sistemelor de Calcul/asm_tools/npp/Lab5EmaB3.asm
@@ -11,37 +11,33 @@ import exit msvcrt.dll    ; exit is a function that ends the calling process. It
 ; our data is declared here (the variables needed by our program)
 segment data use32 class=data
     ; ...
-    ;A string of bytes A is given. Construct string B containing only odd values from string A.
-    ;If A = 17, 4, 2, -2, -1 => B = 17, -1
+    ; A string of words S is given. Compute string D containing only low bytes multiple of 5 from string S.
+    ; If S = 1223h, 5628h => D = 23h, 28h
 
-    A db 17, 4, 2, -2, -1
-    ls equ $ - A
-   
-    B resb ls
-       
+    S dw 1223h, 5628h
+    ls equ ($ - S) / 2; length of S in words
+    D resw ls; reserve space for D
 
 ; our code starts here
 segment code use32 class=code
     start:
-    mov esi, 0
-    mov edi, 0
-    mov ecx, ls
-    mov bl, 2 
         ; ...
-        repeta:
-            movsx ax, byte [A + esi] ; ax = A[esi]
-            idiv bl ; al = ax / bl ; ah = ax % bl
-            cmp ah, 1 ; ah == 1
-            je odd ; if ah == 1 jump to odd
-            jmp end
-        odd:
-            mov al, [A+esi]
-            mov byte [B + edi], al ; B[edi] = al
-            inc edi ; edi++
-        end:
-         inc esi ; esi++
-         loop repeta
-        ; ...
+        mov esi, 0
+        mov edi, 0
+        mov ecx, ls
+        mov bl, 5
+        repeta: 
+            mov ax, [S + esi * 2]; ax = S[esi]
+            idiv bl; al = ax / bl, ah = ax % bl
+            cmp ah, 0; if ah == 0
+            je divizibil; jump to divizibil
+        divizibil:
+            mov [D + edi], ax; D[edi] = ax
+            inc edi
+        final:
+            inc esi
+            loop repeta
+    
         ; exit(0)
         push    dword 0      ; push the parameter for exit onto the stack
         call    [exit]       ; call exit to terminate the program
